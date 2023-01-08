d99f22c4b80e2ec039e65eb38635f2dfa71d7ca2
Chemsou <chemsou.dadine@gmail.com>
2023-01-08 13:35:15 +0100

    added goTop button & scroll

--- js/index.js
+++ js/index.js
@@ -1,12 +1,22 @@
 $(document).ready(function () {
     // ------------- For Change bg-colro of navbar when scroll down ----
     $(window).scroll(function () { 
-        if(this.scrollY > 20) 
+        if(this.scrollY > 20){
             $('.navbar').addClass('sticky');  
-        else
-            $('.navbar').removeClass('sticky');      
+            /* --- goTop Button ---*/
+            $('.goTop').fadeIn();
+        } 
+        else{
+            $('.navbar').removeClass('sticky');
+            /* --- goTop Button Out---*/
+            $('.goTop').fadeOut();      
+        }
     });
 
+    // --------When Click on ICON Arrow-up go on Top Page 0,0 ----
+    $('.goTop').click(function() {
+        scroll(0,0)        
+    });
 
     // ---- For mobile screen when click menu-toggler 
     $('.menu-toggler').click(function () { 
@@ -17,9 +27,8 @@ $(document).ready(function () {
     // ---- For scroll X images on Works section
     $(".works").magnificPopup({
         delegate: 'a',
-        type: "image",
+        type: 'image',
         gallery: {enabled: true}
     });
-
 
 });
